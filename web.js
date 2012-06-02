5c537899a919f23f4df29a051d3cdef4db4149bf
Peter O'Shaughnessy <p_os@yahoo.co.uk>
2012-06-02 11:38:42 +0100

    Getting ready to post slides on Heroku

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.sendfile(__dirname + '/index.html');
+    response.send('coming soon');
+    //response.sendfile(__dirname + '/index.html');
 });
 
 app.configure(function() {
