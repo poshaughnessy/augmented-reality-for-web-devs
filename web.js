40379f74e968e3dc10a709d4316e21c9a6336551
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2018-04-06 14:37:58 +0100

    Upgrade Node & Express and fix

--- web.js
+++ web.js
@@ -1,20 +1,19 @@
 var express = require('express');
+var path = require('path');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 app.get('/', function(request, response) {
-    response.sendfile(__dirname + '/index.html');
+    response.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.configure(function() {
-    app.use('/css', express.static(__dirname + '/css'));
-    app.use('/fonts', express.static(__dirname + '/fonts'));
-    app.use('/img', express.static(__dirname + '/img'));
-    app.use('/js', express.static(__dirname + '/js'));
-    app.use('/media', express.static(__dirname + '/media'));
-    app.use('/models', express.static(__dirname + '/models'));
-    app.use('/favicon.ico', express.static(__dirname + '/favicon.ico'));
-});
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/fonts', express.static(path.join(__dirname, 'fonts')));
+app.use('/img', express.static(path.join(__dirname, 'img')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/media', express.static(path.join(__dirname, 'media')));
+app.use('/models', express.static(path.join(__dirname, 'models')));
+app.use('/favicon.ico', express.static(path.join(__dirname, 'favicon.ico')));
 
 var port = process.env.PORT || 8000;
 app.listen(port, function() {
